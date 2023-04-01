4e0834efc433a59d477fda4034729f94429a7406
NASUNJIN <sj010618@gmail.com>
2023-04-02 04:58:51 +0900

    upload HW5

---
+++ HW5/web_server.py
@@ -0,0 +1,52 @@
+from socket import *
+
+s = socket()
+s.bind(('', 80))
+s.listen(10)
+
+while True:
+    c, addr = s.accept()
+    data = c.recv(1024)
+    msg = data.decode()
+    req = msg.split('\r\n')
+
+    # filename = req[0].split()[1].split('/')[1]  # 공백 분류 + '/'기준 분류
+    filename = req[0].split()[1][1:]
+
+    if filename == 'index.html':
+        f = open(filename, 'r', encoding='utf-8')
+        mimeType = 'text/html'
+        data = f.read()
+        # HTTP 헤더 전송
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(b'Content-Type: ' + mimeType.encode() + b'\r\n')
+        c.send(b'\r\n')
+        c.send(data.encode('euc-kr'))
+
+    elif filename == 'iot.png':
+        f = open(filename, 'rb')
+        mimeType = 'imag/png'
+        data = f.read()
+        # HTTP 헤더 전송
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(b'Content-Type: ' + mimeType.encode() + b'\r\n')
+        c.send(b'\r\n')
+        c.send(data)
+
+    elif filename == 'favicon.ico':
+        f = open(filename, 'rb')
+        mimeType = 'image/x-icon'
+        data = f.read()
+        # HTTP 헤더 전송
+        c.send(b'HTTP/1.1 200 OK\r\n')
+        c.send(b'Content-Type: ' + mimeType.encode() + b'\r\n')
+        c.send(b'\r\n')
+        c.send(data)
+
+    else:
+        c.send(b'HTTP/1.1 404 Not Found\r\n')
+        c.send(b'\r\n')
+        c.send(b'<HTML><HEAD><TITLE>Not Found</TITLE></HEAD>')
+        c.send(b'<BODY>Not Found</BODY></HTML>')
+
+    c.close()
