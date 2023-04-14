a8f7f126222fee90cbf30969c9734123cb17a3ae
NASUNJIN <sj010618@gmail.com>
2023-04-14 20:51:52 +0900

    Chap05

---
+++ Chap05/udp_loss_server.py
@@ -0,0 +1,19 @@
+from socket import *
+import random
+
+BUFF_SIZE = 1024
+port = 5555
+
+s_sock = socket(AF_INET, SOCK_DGRAM)
+s_sock.bind(('', port))
+print('Listening...')
+
+while True:
+    data, addr = s_sock.recvfrom(BUFF_SIZE)
+    # 30%  데이터 손실
+    if random.randint(1, 10) <= 3:
+        print('Packet from {} lost!'.format(addr))
+        continue
+    print('Packet is {} from {}'.format(data.decode(), addr))
+
+    s_sock.sendto('ack'.encode(), addr)
